4fc725bf59be8465d8a1b3eb4805c7bb8c656723
pplmtv <pplmtv@gmail.com>
2023-05-07 20:45:40 +0900

    create_new

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -5,10 +5,10 @@ export default function Layout(props) {
     return (
         <div className="flex flex-col min-h-screen">
             <Navigaion />
-            <main classnName="container flex flex-1 justify-center mx-auto px-5 max-w-screen-lg">
+            <main className="container flex flex-1 justify-center mx-auto px-5 max-w-screen-lg">
                 {props.children}
             </main>
-            <footer clanssName="flex items-center justify-center w-full h-20 text-sm border-t">
+            <footer className="flex items-center justify-center w-full h-20 text-sm border-t">
                 ©︎2023 NextJS Startup
             </footer>
         </div>
