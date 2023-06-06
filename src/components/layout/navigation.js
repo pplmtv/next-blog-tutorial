546ecb1231410bf09b9c8a0a25768c7bf0b8429c
pplmtv <pplmtv@gmail.com>
2023-06-06 23:03:20 +0900

    nextauth

--- src/components/layout/navigation.js
+++ src/components/layout/navigation.js
@@ -1,6 +1,10 @@
-import Link from "next/link";
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link';
 
 export default function Navigation() {
+  // const [session] = useSession()
+  const { data: session } = useSession()
+
   return (
     <header className="container flex flex-row items-center mx-auto px-5 py-14 max-w-screen-lg">
       <Link href="/" className="text-4xl font-bold text-red-300">
@@ -10,6 +14,29 @@ export default function Navigation() {
         <Link href="/about" className="mr-5">
           About
         </Link>
+
+        {session && session.accessToken ? (
+          <>
+              <Link href="/profile" className="mr-5">
+                profile
+              </Link>
+              <div
+                  className="inline-block cursor-pointer"
+                  onClick={() => signOut()}
+              >
+                LOGOUT
+              </div>
+          </>
+        ) : (
+          <>
+              <div
+                  className="inline-block cursor-pointer"
+                  onClick={() => signIn()}
+              >
+                LOGIN
+              </div>
+          </>
+        )}
       </nav>
     </header>
   );
